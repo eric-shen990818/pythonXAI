c83284ac248bbd1241f8a99f588c32aa896a0478
eric-shen990818 <eric.shen990818@gmail.com>
2025-07-01 13:49:18 +0800

    新增 class2-1.py 檔案，包含比較運算子與邏輯運算子的示範

---
+++ pages/class2-1.py
@@ -0,0 +1,36 @@
+# 比較運算子
+
+print(1 == 1)  # true
+print(1 != 1)  # false
+print(1 > 1)  # false
+print(1 < 1)  # false
+print(1 >= 1)  # true
+print(1 <= 1)  # true
+
+# 邏輯運算子
+
+# # and 運算子
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# # or 運算子
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+# # not 運算子
+print(not True)  # False
+print(not False)  # True
+
+# 優先順序
+# 1 () 括號
+# 2 ** 次方
+# 3 * / // % 乘 除 取商 取餘數
+# 4 + - 加 減
+# 5 比較運算子
+# 6 not
+# 7 and
+# 8 or
