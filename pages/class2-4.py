df8f056d229e321bd96b8f968f1e4d79752d6423
eric-shen990818 <eric.shen990818@gmail.com>
2025-07-02 12:05:21 +0800

    新增箭頭金字塔功能，允許使用者輸入數字並顯示對應的箭頭金字塔形狀

--- pages/class2-4.py
+++ pages/class2-4.py
@@ -1,7 +1,20 @@
 import streamlit as st
 
-st.title("數字金字塔")
-number = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
-st.write("數字金字塔:")
-for i in range(1, number + 1):
-    st.write(f"{i}" * i)
+# st.title("數字金字塔")
+# number = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
+# st.write("數字金字塔:")
+# for i in range(1, number + 1):
+#     st.write(f"{i}" * i)
+
+st.title("箭頭金字塔")
+number2 = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
+a = ""
+for i in range(1, number2 + 1):
+    spaces = " " * (number2 - i)
+    stars = "*" * (2 * i - 1)
+    a = a + (spaces + stars + "\n")
+for j in range(1, number2 + 1):
+    starline = "*"
+    space = " " * (number2 - 1)
+    a = a + space + starline + "\n"
+st.markdown(f"```箭頭金字塔：\n{a}")
