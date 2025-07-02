51838d6ba396200ea4bed6d0740b81746a9d36b0
eric-shen990818 <eric.shen990818@gmail.com>
2025-07-02 13:34:16 +0800

    新增數字金字塔功能，允許使用者輸入數字並顯示對應的金字塔形狀 新增 Class2 課堂筆記，展示 list 的基本用法及元素讀取

--- pages/class2-4.py
+++ pages/class2-4.py
@@ -1,10 +1,11 @@
 import streamlit as st
 
-# st.title("數字金字塔")
-# number = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
-# st.write("數字金字塔:")
-# for i in range(1, number + 1):
-#     st.write(f"{i}" * i)
+st.title("數字金字塔")
+number = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
+st.write("數字金字塔:")
+for i in range(1, number + 1):
+    st.write(f"{i}" * i)
+
 
 st.title("箭頭金字塔")
 number2 = st.number_input("請輸入一個數字", min_value=1, max_value=9, step=1)
---
+++ pages/class2-5.py
@@ -0,0 +1,12 @@
+print([])  # 這是一個空的list
+print([1, 2, 3])  # 這是一個三個元素的list
+print([1, 2, 3, "a", "b", "c"])  # 這是一個有六個元素的list
+print([1, 2, 3, ["a", "b", "c"]])  # 這是一個有四個元素的list
+print([1, True, "a", 1.23])  # 這是一個有四個元素的list
+
+# list 讀取元素，元素的index從0開始
+L = [1, 2, 3, "a", "b", "c"]
+print(L[0])  # 1
+print(L[1])  # 2
+print(L[2])  # 3
+print(L[3])  # "a"
