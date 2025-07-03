22c0df2d72c18aad3b3b2c46f67523ff5342c16e
eric-shen990818 <eric.shen990818@gmail.com>
2025-07-03 11:14:51 +0800

    修正按鈕函數名稱，將botton更正為button

--- pages/class3-1.py
+++ pages/class3-1.py
@@ -41,16 +41,16 @@ import streamlit as st
 
 st.title("欄位元件")
 col1, col2 = st.columns(2)  # 2columns
-col1.botton("按鈕1", key="btn1")  # 在col1建立一個按鈕類似st.botton("按鈕1")
+col1.button("按鈕1", key="btn1")  # 在col1建立一個按鈕類似st.botton("按鈕1")
 col2.button("按鈕2", key="btn2")  # 在col2建立一個按鈕類似st.botton("按鈕2")
 
 # 2columns,可以用比例來設定每個cloumn的寬度,將比例放到list中
 col1, col2 = st.columns([1, 2])
-col1.botton("按鈕1", key="btn3")  # 在col1建立一個按鈕類似st.botton("按鈕1")
+col1.button("按鈕1", key="btn3")  # 在col1建立一個按鈕類似st.botton("按鈕1")
 col2.button("按鈕2", key="btn4")  # 在col2建立一個按鈕類似st.botton("按鈕2")
 
 # 3columns
 col1, col2, col3 = st.columns([1, 2, 3])
-col1.botton("按鈕1", key="btn5")  # 在col1建立一個按鈕類似st.botton("按鈕1")
+col1.button("按鈕1", key="btn5")  # 在col1建立一個按鈕類似st.botton("按鈕1")
 col2.button("按鈕2", key="btn6")  # 在col2建立一個按鈕類似st.botton("按鈕2")
 col3.button("按鈕3", key="btn7")  # 在col3建立一個按鈕類似st.botton("按鈕3")
